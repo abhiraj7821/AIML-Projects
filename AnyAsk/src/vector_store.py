5ff0c04b58b883100fe0eab62b5f7f57f4e38bc0
Abhishek Rajput <abhiraj7821@gmail.com>
2025-04-20 09:38:37 +0530

    Bug resolving
    Bug resolving

--- AnyAsk/src/vector_store.py
+++ AnyAsk/src/vector_store.py
@@ -12,16 +12,6 @@ import warnings
 # Displaying a warning message
 warnings.warn('Error: A warning just appeared')
 
-# documents='''
-# Nvidia's fourth-quarter results, released on February 26, 2025, showed a record revenue of $39.3 billion, up 12% from the previous quarter and 78% from the previous year. This performance beat Wall Street expectations on both revenue and earnings per share. The company's Data Center revenue also reached a record $35.6 billion, up 16% quarter-over-quarter and 93% year-over-year.
-# Key Highlights:
-# Revenue: $39.3 billion, up 12% QoQ and 78% YoY.
-# Data Center Revenue: $35.6 billion, up 16% QoQ and 93% YoY.
-# Earnings per Share: $0.89 (beat expectations of $0.84).
-# Full-year Revenue: $130.5 billion, up 114%.
-# These results reflect a strong performance driven by the growing demand for NVIDIA's products, particularly in the AI and data center sectors. The company's Blackwell platform is also seeing strong demand and rapid ramp-up.
-# '''
-
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 model_name="sentence-transformers/all-mpnet-base-v2"
 model_kwargs = {'device': DEVICE}
@@ -52,6 +42,17 @@ def create_vector_store(texts):
     print("VECTOR STORE CREATED")
     return vector_store
     
+
+
+# documents='''
+# Nvidia's fourth-quarter results, released on February 26, 2025, showed a record revenue of $39.3 billion, up 12% from the previous quarter and 78% from the previous year. This performance beat Wall Street expectations on both revenue and earnings per share. The company's Data Center revenue also reached a record $35.6 billion, up 16% quarter-over-quarter and 93% year-over-year.
+# Key Highlights:
+# Revenue: $39.3 billion, up 12% QoQ and 78% YoY.
+# Data Center Revenue: $35.6 billion, up 16% QoQ and 93% YoY.
+# Earnings per Share: $0.89 (beat expectations of $0.84).
+# Full-year Revenue: $130.5 billion, up 114%.
+# These results reflect a strong performance driven by the growing demand for NVIDIA's products, particularly in the AI and data center sectors. The company's Blackwell platform is also seeing strong demand and rapid ramp-up.
+# '''
 
 # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 # texts = text_splitter.split_text(documents)
